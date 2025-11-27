717f03e2b5dc7dc6b327cd9b9ceb910926fe2bb1
Sylwia Wolańska <s30753@pjwstk.edu.pl>
2025-11-27 14:35:42 +0100

    #1 Initial commit - main issues fixed

---
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -0,0 +1,27 @@
+﻿// In case you need some guidance: https://refactoring.guru/design-patterns/adapter
+namespace DesignPattern.Adapter
+{
+    public class EmployeeAdapter : ITarget
+    {
+        private readonly BillingSystem thirdPartyBillingSystem = new();
+
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            List<Employee> employeeList = new List<Employee>();
+
+            int rows = employeesArray.GetLength(0);
+
+            for (int i = 0; i < rows; i++)
+            {
+                int id = int.Parse(employeesArray[i, 0]);
+                string name = employeesArray[i, 1];
+                string designation = employeesArray[i, 2];
+                decimal salary = decimal.Parse(employeesArray[i, 3]);
+
+                employeeList.Add(new Employee(id, name, designation, salary));
+            }
+
+            thirdPartyBillingSystem.ProcessSalary(employeeList);
+        }
+    }
+}
---
+++ DesignPattern/Factory/DatabaseFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPattern.Factory
+{
+    public static class DatabaseFactory
+    {
+        public static IDatabase CreateDatabase(DatabaseType type)
+        {
+            switch (type)
+            {
+                case DatabaseType.SqlServer:
+                    return new SqlServerDatabase();
+
+                case DatabaseType.Oracle:
+                    return new OracleDatabase();
+
+                default:
+                    throw new ArgumentException("Unsupported database type");
+            }
+        }
+    }
+}
